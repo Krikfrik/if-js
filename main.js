5a31d3ff90d851c82855e48c9de2054e03706a55
Krikfrik <bennaginger21@gmail.com>
2021-05-25 00:22:18 +0300

    Homework done

---
+++ main.js
@@ -0,0 +1,39 @@
+let user = 'John Doe';
+console.log(user);
+let student = 'Hanna Hirkouskaya';
+console.log(student);
+user = student;
+//в консоли Hanna Hirkouskaya
+console.log(user);
+
+let test = 1;
+test = test + '1';
+//в консоли 1 
+console.log(test);
+test = test - 1;
+// в консоли значение 10;
+console.log(test);
+test = Boolean(test);
+// в консоли true
+console.log(test);
+
+const arr = [2, 3, 5, 8];
+let resalt = 0;
+for(let i = 0; i < arr.length; ++i){
+    resalt += arr[i];
+}
+console.log(resalt);
+
+const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+for(let i = 0; i < arr2.length; ++i){
+    if(arr2[i] > 5 && arr2[i] < 10){
+        console.log(arr2[i])
+    }
+}
+
+for(let i = 0; i < arr2.length; ++i){
+    if(!(arr2[i] % 2)){
+        console.log(arr2[i])
+    }
+}
+
