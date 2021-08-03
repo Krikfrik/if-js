c79c6b1d5727f86b102c3c8fabcada42ddebd5ce
Krikfrik <bennaginger21@gmail.com>
2021-08-03 22:30:26 +0300

    uncommented data items

--- TripHouse/TripHouse.js
+++ TripHouse/TripHouse.js
@@ -23,51 +23,53 @@ const data = [
       country: 'Germany',
       imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
     },
-    // {
-    //   name: 'Radisson Blu Hotel',
-    //   city: 'Kyiv',
-    //   country: 'Ukraine',
-    //   imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
-    // },
-    // {
-    //   name: 'Paradise Hotel',
-    //   city: 'Guadalupe',
-    //   country: 'Mexico',
-    //   imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
-    // },
-    // {
-    //   name: 'Hotel Grindewald',
-    //   city: 'Interlaken',
-    //   country: 'Switzerland',
-    //   imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
-    // },
-    // {
-    //   name: 'The Andaman Resort',
-    //   city: 'Port Dickson',
-    //   country: 'Malaysia',
-    //   imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
-    // },
+    {
+      name: 'Radisson Blu Hotel',
+      city: 'Kyiv',
+      country: 'Ukraine',
+      imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/radisson-blu-hotel_jwtowg.jpg',
+    },
+    {
+      name: 'Paradise Hotel',
+      city: 'Guadalupe',
+      country: 'Mexico',
+      imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/paradise-hotel_i6whae.jpg',
+    },
+    {
+      name: 'Hotel Grindewald',
+      city: 'Interlaken',
+      country: 'Switzerland',
+      imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-grindewald_zsjsmy.jpg',
+    },
+    {
+      name: 'The Andaman Resort',
+      city: 'Port Dickson',
+      country: 'Malaysia',
+      imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/the-andaman-resort_d2xksj.jpg',
+    },
 ];
 
 function getListContent() {
   let result = [];
   
-  data.forEach((item) => {
-    const homesItem = document.createElement('li');
-    homesItem.className = 'Homes_item';
-    const homesPictureLink = document.createElement ("a")
-    const homesPicture = document.createElement("img");
-    homesPicture.className = 'Homes_picture';
-    homesPicture.setAttribute("src", `${item.imageUrl}`);
-    homesPictureLink.append(homesPicture);
-    const homesName = document.createElement("a");
-    homesName.className = 'Homes_name';
-    const homesNameText = document.createTextNode(`${item.name}`);
-    homesName.appendChild(homesNameText);
-    const homesAdress = document.createTextNode(`${item.city}, ${item.country}`);
-    homesAdress.className = 'Homes_adress';
-    homesItem.append(homesPictureLink, homesName, homesAdress);
-    result.push(homesItem);
+  data.forEach((item, index) => {
+    if(index <= 3){
+      const homesItem = document.createElement('li');
+      homesItem.className = 'Homes_item';
+      const homesPictureLink = document.createElement ("a")
+      const homesPicture = document.createElement("img");
+      homesPicture.className = 'Homes_picture';
+      homesPicture.setAttribute("src", `${item.imageUrl}`);
+      homesPictureLink.append(homesPicture);
+      const homesName = document.createElement("a");
+      homesName.className = 'Homes_name';
+      const homesNameText = document.createTextNode(`${item.name}`);
+      homesName.appendChild(homesNameText);
+      const homesAdress = document.createTextNode(`${item.city}, ${item.country}`);
+      homesAdress.className = 'Homes_adress';
+      homesItem.append(homesPictureLink, homesName, homesAdress);
+      result.push(homesItem);
+    }
   })
 
   return result;
